91ac258cb24c7a4a0494c03649eb8f628da0b45e
Josephine Pockele <j.s.pockele@tudelft.nl>
2024-01-18 14:01:20 +0100

    Moved the App and ScreenManager to the GUI package (#7)

--- GUI/__init__.py
+++ GUI/__init__.py
@@ -1,6 +1,28 @@
 from kivy.lang import Builder
+from kivy.app import App
+from kivy.core.window import Window
+from kivy.uix.screenmanager import ScreenManager
 
 from .audio_question_screen import *
 
 
 Builder.load_file('GUI/setup.kv')
+
+
+class PalilaScreenManager(ScreenManager):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+
+class PalilaApp(App):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def build(self):
+        Window.clearcolor = (.9, .9, .9, 1)
+        Window.size = (1600, 900)
+
+        manager = PalilaScreenManager()
+        manager.add_widget(AudioQuestionScreen())
+
+        return manager
--- main.py
+++ main.py
@@ -1,27 +1,4 @@
-from kivy.app import App
-from kivy.core.window import Window
-from kivy.uix.screenmanager import ScreenManager
-
-from GUI import AudioQuestionScreen
-
-
-class PalilaScreenManager(ScreenManager):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-
-
-class PalilaApp(App):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-
-    def build(self):
-        Window.clearcolor = (.9, .9, .9, 1)
-        Window.size = (1600, 900)
-
-        manager = PalilaScreenManager()
-        manager.add_widget(AudioQuestionScreen())
-
-        return manager
+from GUI import PalilaApp
 
 
 if __name__ == '__main__':
