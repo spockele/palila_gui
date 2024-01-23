10407b168d1ab8e00063312389b92a0e204f65bb
Josephine Pockele <j.s.pockele@tudelft.nl>
2024-01-23 13:52:41 +0100

    Comments are nice (#7)

--- GUI/__init__.py
+++ GUI/__init__.py
@@ -11,32 +11,57 @@ Builder.load_file('GUI/setup.kv')
 
 
 class PalilaScreenManager(ScreenManager):
+    """
+    Subclass of kivy ScreenManager which handles the connection between the GUI and the input file system.
+    """
     def __init__(self, experiment: PalilaExperiment, **kwargs):
+        """
+        :param experiment: An instance of PalilaExperiment containing the configuration for the ScreenManager.
+        """
         super().__init__(**kwargs)
-
         self.experiment = experiment
+
+        # Extract the experiment sections
         self.sections = [section for section in self.experiment.sections if 'section' in section]
+        # Go about initialising the Screens based on the input file
+        self._initialise_screens()
 
-        self._organise()
-
-    def _organise(self):
+    def _initialise_screens(self):
+        """
+        Internal function to initialise the Screens from the PalilaExperiment instance
+        """
+        # Loop over the experiment sections
         for section in self.sections:
             audios = [audio for audio in self.experiment[section].sections if 'audio' in audio]
+            # Within each section, loop over the audios
             for audio in audios:
+                # Gather the corresponding configuration dictionary and add the general audio path of the experiment
                 audio_config_dict = self.experiment[section][audio]
                 audio_config_dict['filepath'] = self.experiment.audio_path
-
+                # Create the screen and add to the Manager
                 self.add_widget(AudioQuestionScreen(audio_config_dict))
 
 
 class PalilaApp(App):
-    def __init__(self, experiment_name, **kwargs):
+    """
+    Subclass of kivy App for the customisation needed for the GUI
+    """
+    def __init__(self, experiment_name: str, **kwargs):
+        """
+        :param experiment_name: name of the experiment configuration file (without the .palila extension
+        """
         super().__init__(**kwargs)
 
+        # Load the experiment from the file
         self.experiment = PalilaExperiment(experiment_name)
 
     def build(self):
-        Window.clearcolor = (.9, .9, .9, 1)
+        """
+        Builds the App (default App function)
+        """
+        # Set the screen color to a very light grey
+        Window.clearcolor = (.95, .95, .95, 1)
+        # Set the screen to a fixed resolution
         Window.size = (1600, 900)
 
         manager = PalilaScreenManager(self.experiment)
