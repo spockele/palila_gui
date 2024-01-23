1915ad4533721029549294db2a9b44b2e2296025
Josephine Pockele <j.s.pockele@tudelft.nl>
2024-01-23 10:08:28 +0100

    Beginning to integrate the file input system into the ScreenManager (#5, #7)

--- GUI/__init__.py
+++ GUI/__init__.py
@@ -11,19 +11,28 @@ Builder.load_file('GUI/setup.kv')
 
 
 class PalilaScreenManager(ScreenManager):
-    def __init__(self, **kwargs):
+    def __init__(self, experiment: PalilaExperiment, **kwargs):
         super().__init__(**kwargs)
+
+        self.experiment = experiment
+
+        self._organise()
+
+    def _organise(self):
+        pass
 
 
 class PalilaApp(App):
-    def __init__(self, **kwargs):
+    def __init__(self, experiment_name, **kwargs):
         super().__init__(**kwargs)
+
+        self.experiment = PalilaExperiment(experiment_name)
 
     def build(self):
         Window.clearcolor = (.9, .9, .9, 1)
         Window.size = (1600, 900)
 
-        manager = PalilaScreenManager()
+        manager = PalilaScreenManager(self.experiment)
         manager.add_widget(AudioQuestionScreen())
 
         return manager
--- main.py
+++ main.py
@@ -2,4 +2,4 @@ from GUI import PalilaApp
 
 
 if __name__ == '__main__':
-    PalilaApp().run()
+    PalilaApp('gui_dev').run()
