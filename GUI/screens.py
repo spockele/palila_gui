ac2b05e5d16232ee74ab7ae32e3ecffce691a446
Josephine Pockele <j.s.pockele@tudelft.nl>
2024-01-25 18:25:59 +0100

    Moved the navigation buttons to the general PalilaScreen class

--- GUI/screens.py
+++ GUI/screens.py
@@ -1,14 +1,11 @@
 from kivy.uix.screenmanager import Screen
 
 
+__all__ = ['PalilaScreen']
+
+
 class PalilaScreen(Screen):
-    def __init__(self, **kwargs):
+    def __init__(self, previous_screen: str, next_screen: str, **kwargs):
         super().__init__(**kwargs)
-        self.next = None
-        self.previous = None
-
-    def set_next(self, next_screen: Screen):
-        self.next = next_screen
-
-    def set_previous(self, previous_screen: Screen):
-        self.previous = previous_screen
+        self.previous_screen = previous_screen
+        self.next_screen = next_screen
