55e5161c6f34517fe505e15ef1564de6619e5bf7
Josephine Pockele <j.s.pockele@tudelft.nl>
2024-01-30 18:00:00 +0100

    The compass is back baby, yeah :) (#8)

--- GUI/audio_questions.py
+++ GUI/audio_questions.py
@@ -1,7 +1,7 @@
-from kivy.lang import Builder
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.widget import Widget
 from kivy.uix.button import Button
+from kivy.lang import Builder
 
 Builder.load_file('GUI/audio_questions.kv')
 
@@ -11,7 +11,7 @@ class Filler(Widget):
 
 
 class ChoiceButton(Button):
-    def __init__(self, choice: str, **kwargs):
+    def __init__(self, choice: str = '', **kwargs):
         super().__init__(text=choice, **kwargs)
 
     def select(self) -> None:
@@ -87,6 +87,31 @@ class NumScaleQuestion(BoxLayout):
             button.pos_hint = pos_hint
 
         self.current_answer = None
+
+    def select_choice(self, choice: ChoiceButton):
+        if self.current_answer is not None:
+            self.current_answer.deselect()
+
+        if self.current_answer == choice:
+            self.current_answer = None
+        else:
+            self.current_answer = choice
+
+
+class PointCompassQuestion(BoxLayout):
+    def __init__(self, question_dict: dict, **kwargs):
+        super().__init__(**kwargs)
+
+        self.question_dict = question_dict
+        self.ids.question_text.text = question_dict['text']
+
+        self.current_answer = None
+
+    def on_size(self, *_):
+        if self.parent.n_question > 1:
+            raise OverflowError('PointCompass question takes 2 question slots.')
+
+        self.parent.n_max = 1
 
     def select_choice(self, choice: ChoiceButton):
         if self.current_answer is not None:
