299d5cdd4969fccf8c44a2e20b9c8ed4578f5122
Josephine Pockele <j.s.pockele@tudelft.nl>
2024-02-07 13:57:51 +0100

    Making the choice only lowercase so they actually work properly

--- update.bat
+++ update.bat
@@ -2,23 +2,23 @@
 
 set /P choice="Which updates do you want to get? Python only (P), GUI only (G), both (press enter): "
 
-if (%choice%==P or %choice%==p) or (not (defined %choice%)) (
-REM Script to update all installed python packages and pip
-REM  1) Update pip and setuptools
-venv\Scripts\python.exe -m pip install --upgrade pip setuptools
-REM  2) Gather installed packages
-venv\Scripts\python.exe -m pip freeze > update.txt
-REM  3) Format the obtained file to force update
-venv\Scripts\python.exe -m update_helper
-REM  4) Actually update the packages with pip
-venv\Scripts\python.exe -m pip install --upgrade -r update.txt
-REM  5) Remove the update file
-del update.txt
+if %choice%==p (
+	REM Script to update all installed python packages and pip
+	REM  1) Update pip and setuptools
+	venv\Scripts\python.exe -m pip install --upgrade pip setuptools
+	REM  2) Gather installed packages
+	venv\Scripts\python.exe -m pip freeze > update.txt
+	REM  3) Format the obtained file to force update
+	venv\Scripts\python.exe -m update_helper
+	REM  4) Actually update the packages with pip
+	venv\Scripts\python.exe -m pip install --upgrade -r update.txt
+	REM  5) Remove the update file
+	del update.txt
 )
 
-if (%choice%==G or %choice%==g) or (not (defined %choice%)) ()
-REM Also update the repo by pulling the latest
-git pull origin main
+if %choice%==g (
+	REM Also update the repo by pulling the latest
+	git pull origin main
 )
 
 echo.
