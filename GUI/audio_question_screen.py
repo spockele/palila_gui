db77b5c00706ce016a681ea2688f4c615162f914
Josephine Pockele <j.s.pockele@tudelft.nl>
2024-02-15 17:36:45 +0100

    Moved the progressbar thread to its new home

--- GUI/audio_question_screen.py
+++ GUI/audio_question_screen.py
@@ -1,40 +1,15 @@
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.progressbar import ProgressBar
 from kivy.core.audio import SoundLoader
 from kivy.lang import Builder
 
-import threading
-import time
-
 from .screens import PalilaScreen, Filler
+from .threaded_tools import ProgressBarThread
 from . import audio_questions
 
 
 __all__ = ['AudioQuestionScreen']
+
 Builder.load_file('GUI/audio_question_screen.kv')
-
-
-class ProgressBarThread(threading.Thread):
-    """
-    Thread subclass to manage the ProgressBar that times audio
-    """
-    def __init__(self, progress_bar: ProgressBar, **kwargs):
-        """
-        @param progress_bar: The ProgressBar object to be timed
-        """
-        super().__init__(**kwargs)
-        self.progress_bar = progress_bar
-
-    def run(self):
-        # Set initial time
-        t0 = time.time()
-        dt = .1
-        # Do while the time is below the max of the progress bar
-        while time.time() - t0 <= self.progress_bar.max + dt:
-            # Update the progress bar value
-            self.progress_bar.value = time.time() - t0
-            # # Hold to not overload the system
-            time.sleep(dt)
 
 
 class AudioManager(BoxLayout):
---
+++ GUI/threaded_tools.py
@@ -0,0 +1,27 @@
+import threading
+import time
+
+from kivy.uix.progressbar import ProgressBar
+
+
+class ProgressBarThread(threading.Thread):
+    """
+    Thread subclass to manage the ProgressBar that times audio
+    """
+    def __init__(self, progress_bar: ProgressBar, **kwargs):
+        """
+        @param progress_bar: The ProgressBar object to be timed
+        """
+        super().__init__(**kwargs)
+        self.progress_bar = progress_bar
+
+    def run(self):
+        # Set initial time
+        t0 = time.time()
+        dt = .1
+        # Do while the time is below the max of the progress bar
+        while time.time() - t0 <= self.progress_bar.max + dt:
+            # Update the progress bar value
+            self.progress_bar.value = time.time() - t0
+            # # Hold to not overload the system
+            time.sleep(dt)
