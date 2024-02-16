914847b933ea5db4d1a8b13819f272866725ce4b
Josephine Pockele <j.s.pockele@tudelft.nl>
2024-02-16 17:35:50 +0100

    Documented the main module of the GUI package (#18)

--- GUI/__init__.py
+++ GUI/__init__.py
@@ -1,3 +1,9 @@
+"""
+Main module of the GUI package.
+
+Pulls in all functions from the modules in the package. It also defines the custom ScreenManager and App classes, which
+form the core of this package. Loads all the relevant kivy files for this package.
+"""
 from kivy.uix.screenmanager import ScreenManager
 from kivy.core.window import Window
 from kivy.lang import Builder
@@ -8,17 +14,38 @@ from .questionnaire import *
 from .file_system import *
 from .screens import *
 
+
+__all__ = ['PalilaApp', ]
+
+Builder.load_file('GUI/audio_question_screen.kv')
+Builder.load_file('GUI/audio_questions.kv')
+Builder.load_file('GUI/numpad_bubble.kv')
+Builder.load_file('GUI/questionnaire.kv')
 Builder.load_file('GUI/setup.kv')
 
 
 class PalilaScreenManager(ScreenManager):
     """
-    Subclass of kivy ScreenManager which handles the connection between the GUI and the input file system.
+    Subclass of kivy.uix.screenmanager.ScreenManager which handles the connection between the GUI and the input file system.
+
+    Parameters
+    ----------
+    experiment : PalilaExperiment
+        PalilaExperiment instance to link this screen manager to.
+    answers : PalilaAnswers
+        PalilaAnswers instance to link this screen manager to.
+    **kwargs : dict
+        Keyword arguments. These are passed on to the kivy.uix.screenmanager.ScreenManager constructor.
+
+    Attributes
+    ----------
+    experiment : PalilaExperiment
+        The linked PalilaExperiment instance.
+    answers : PalilaAnswers
+        The linked PalilaAnswers instance.
     """
-    def __init__(self, experiment: PalilaExperiment, answers: PalilaAnswers, **kwargs):
-        """
-        :param experiment: An instance of PalilaExperiment containing the configuration for the ScreenManager.
-        """
+
+    def __init__(self, experiment: PalilaExperiment, answers: PalilaAnswers, **kwargs) -> None:
         super().__init__(**kwargs)
         self.experiment = experiment
         self.answers = answers
@@ -30,7 +57,7 @@ class PalilaScreenManager(ScreenManager):
         # Go about initialising the Screens based on the input file
         self._initialise_screens()
 
-    def _initialise_screens(self):
+    def _initialise_screens(self) -> None:
         """
         Internal function to initialise the Screens from the PalilaExperiment instance
         """
@@ -55,48 +82,72 @@ class PalilaScreenManager(ScreenManager):
 
         self.add_widget(PalilaScreen('', '', name='end'))
 
-    def navigate_next(self):
+    def navigate_next(self) -> None:
         """
-
+        Navigate to the next screen, based on the string defined in the current screen.
         """
         self.transition.direction = 'left'
         self.current = self.current_screen.next_screen
 
-    def navigate_previous(self):
+    def navigate_previous(self) -> None:
         """
-
+        Navigate to the previous screen, based on the string defined in the current screen.
         """
         self.transition.direction = 'right'
         self.current = self.current_screen.previous_screen
 
-    def set_pid(self, pid: str):
+    def set_pid(self, pid: str) -> None:
         """
+        Pass-through function to set the participant ID in the linked PalilaAnswers instance.
 
+        Parameters
+        ----------
+        pid : str
+            The participant ID to be set.
         """
-        return self.answers.set_pid(pid)
+        self.answers.set_pid(pid)
 
-    def store_answer(self, key: str, value):
+    def store_answer(self, key: str, value: str) -> None:
         """
+        Store an answer in the pandas.DataFrame in the linked PalilaAnswers instance.
 
+        Parameters
+        ----------
+        key : str
+            Column key in the answers DataFrame. Should be the Question ID of the requesting question.
+        value : str
+            The answer value to be stored in the DataFrame. Should be a string to avoid issues with pandas.
         """
         self.answers.out.loc['response', key] = value
 
 
 class PalilaApp(App):
     """
-    Subclass of kivy App for the customisation needed for the GUI
+    Subclass of kivy.app.App for the customisation needed for the GUI.
+
+    Parameters
+    ----------
+    experiment_name : str
+        Name of the listening experiment config file (<name>.palila) and directory.
+    **kwargs : dict
+        Keyword arguments. These are passed on to the kivy.app.App constructor.
+
+    Attributes
+    ----------
+    experiment : PalilaExperiment
+        The linked PalilaExperiment instance.
+    answers : PalilaAnswers
+        The linked PalilaAnswers instance.
     """
-    def __init__(self, experiment_name: str, **kwargs):
-        """
-        :param experiment_name: name of the experiment configuration file (without the .palila extension
-        """
+
+    def __init__(self, experiment_name: str, **kwargs) -> None:
         super().__init__(**kwargs)
 
         # Load the experiment from the file
         self.experiment = PalilaExperiment(experiment_name)
         self.answers = PalilaAnswers(self.experiment)
 
-    def build(self):
+    def build(self) -> PalilaScreenManager:
         """
         Builds the App (default App function)
         """
@@ -111,9 +162,9 @@ class PalilaApp(App):
         # Required return of the ScreenManager
         return manager
 
-    def on_stop(self):
+    def on_stop(self) -> None:
         """
-
+        Actions to be taken when the app is stopped.
         """
         if self.experiment.name != 'gui_dev':
-            self.answers.out.to_csv(self.answers.out_path)
+            self.answers.save_to_file()
