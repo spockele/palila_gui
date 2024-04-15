4e6f79d34af9f8176c6a89c3594109c045fc632e
Josephine Pockele <j.s.pockele@tudelft.nl>
2024-04-15 10:00:09 +0200

    Moved the kivy code for the questionnaire screen (#23)

--- GUI/__init__.py
+++ GUI/__init__.py
@@ -23,6 +23,7 @@ Builder.load_file('GUI/audio_question_screen.kv')
 Builder.load_file('GUI/audio_questions.kv')
 Builder.load_file('GUI/numpad_bubble.kv')
 Builder.load_file('GUI/questionnaire_questions.kv')
+Builder.load_file('GUI/questionnaire_screen.kv')
 Builder.load_file('GUI/setup.kv')
 
 
