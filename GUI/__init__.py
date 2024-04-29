40e50f08e3b6f2299119f926fbb9ba76d99f35cf
Josephine Pockele <j.s.pockele@tudelft.nl>
2024-04-29 12:58:44 +0200

    Started with docstring work in this module (#46)

--- GUI/__init__.py
+++ GUI/__init__.py
@@ -13,10 +13,14 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 
+------------------------------------------------------------------------------------------------------------------------
+
 Main module of the GUI package.
 
 Pulls in all functions from the modules in the package. It also defines the custom ScreenManager and App classes, which
 form the core of this package. Loads all the relevant kivy files for this package.
+
+------------------------------------------------------------------------------------------------------------------------
 """
 from kivy.uix.screenmanager import ScreenManager
 from kivy.core.window import Window
