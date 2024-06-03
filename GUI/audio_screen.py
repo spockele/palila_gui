98f4c74acd3a6a924da5a3a7bd0b0a97ff66268f
Josephine Pockele <j.s.pockele@tudelft.nl>
2024-06-03 11:27:53 +0200

    Fixed the module docstring (#48)

--- GUI/audio_screen.py
+++ GUI/audio_screen.py
@@ -13,7 +13,11 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 
+------------------------------------------------------------------------------------------------------------------------
+
 Module containing the overall classes for the Audio Question screens.
+
+------------------------------------------------------------------------------------------------------------------------
 """
 from kivy.uix.boxlayout import BoxLayout
 from kivy.core.audio import SoundLoader
