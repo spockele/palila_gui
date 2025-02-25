5c946be0c044d30552bcf37f2dea817bee3faf3a
Josephine Pockele <j.s.pockele@tudelft.nl>
2025-02-25 09:17:56 +0100

    Putting audio playing to always block continue

--- GUI/audio_screen.py
+++ GUI/audio_screen.py
@@ -130,9 +130,9 @@ class AudioQuestionScreen(QuestionScreen):
         """
         audio_state_left = self.audio_manager_left.count >= 1
         audio_state_right = self.audio_manager_right.n_max is None or self.audio_manager_right.count >= 1
-        audio_state = audio_state_left and audio_state_right and not self.audio_playing
+        audio_state = audio_state_left and audio_state_right
 
-        if audio_state or self.state_override:
+        if audio_state or self.state_override and not self.audio_playing:
             self.question_manager.unlock()
             self.ids.extra_message.text = ''
 
